07c70f82a26c2cbcc1b42558b9e14eaa0e44238d
Arka Ray <25000887+rayarka@users.noreply.github.com>
2021-01-23 02:14:00 +0800

    Updated readme and improved print output

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# TrumpTwitterDoppelgangerDetector
+# Is your twitter friend actually Donald Trump in disguise?
+
+After twitter banned @therealdonaldtrump, the American president went on to tweet from different twitter accounts related to the Trump campaign. Apparently, his media manager - Gary Coby (@garycoby) - offered President trump his own account. Twitter noticed and swiftly banned @garycoby as well.
+
+I believe that the Trump campaign will have learnt from that event. So now, if ex-President Trump wishes to use someone else's account secretly, they won't announce it, so that they can post undettered.
+
+So who's to say that your friend isn't actually <b>Donald Trump</b>?
+
+## There's only 1 way to find out!
+
+While he can change his profile details, we can assume that his tweets will remain somewhat similar! Humans are, after all, creatures of habit. So how can we figure out if your friend is secretly Donald Trump?
+
+<i><b>Simple!</b></i>
+
+We will see your friend's (or any public account's) recent tweets! And if they're similar to what DJT would have posted, I have some bad news for you :( We will be using the Levenshtein algorithm to compare your friends tweets to all of DJT's tweets over the past 6 months!
+
+Simply run the program (assuming you have Twitter's API keys) and enter your suspect's username :)
--- trumpdetection.py
+++ trumpdetection.py
@@ -5,13 +5,16 @@ import webbrowser
 import time
 from twitterauthentication import appAPIKey, appAPIKeySecret
 
+#LOGGING
 start = time.time()
 
-trump_csv = "mid20to2021_trump_tweets.csv"
-consumer_key = appAPIKey
-consumer_secret = appAPIKeySecret
-callback_uri = 'oob'
-max_tweets = 30
+#Authentication Details
+consumer_key = appAPIKey    #Get from Twitter
+consumer_secret = appAPIKeySecret   #Get from Twitter
+callback_uri = 'oob'    #Unsure about how to implement but apparently if you're not doing this on some website, you can use 'oob' to handle it
+
+trump_csv = "mid20to2021_trump_tweets.csv"  #Reference to file where trump tweets are stored
+max_tweets = 30 #Number of tweets from user-provided account to analyse. Given that the trump tweet dataset has 1984 values, each extra tweet means that the string similarity algorithm runs 1984 times more.
 
 def authentication(consumer_key, consumer_secret, callback_uri):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_uri)
@@ -31,12 +34,14 @@ def authentication(consumer_key, consumer_secret, callback_uri):
         return api
 
 def user_data_parsing(api, username, max_tweets):
+    print("Hmm... seems suspicious. Let me check it out!")
     user_corpus = []
     for tweet in tweepy.Cursor(api.user_timeline,id=username).items(max_tweets):
         user_corpus.append(tweet.text.split("https")[0])
     return user_corpus
 
 def trump_data_parsing(csvfile):
+    print("Hmm highly odd...")
     trump_fulltweetdata = pd.read_csv(csvfile)
     trump_corpus = trump_fulltweetdata['text'].str.split("https", expand=True)
     trump_corpus = trump_corpus[0]
@@ -53,58 +58,34 @@ def data_comparison(user_corpus, trump_corpus):
                     'UserText': j
                 }
             )
+            if len(list_of_scores)%10000 == 0:
+                print("Processing...")
     return list_of_scores
 
-def user_similarity_verdict(scores, percentage_confidence = 90):
+def user_similarity_verdict(username, scores, percentage_confidence):
     ldf = pd.DataFrame(scores)
     ldf = ldf.sort_values(by="ratio", ascending=False)
-    # print(ldf_sorted.head())
-    print(ldf[ldf['ratio']>=percentage_confidence].shape[0], "out of", ldf.shape[0], f"at or above {percentage_confidence}% similar")
-    return 100 * ldf[ldf['ratio']>=percentage_confidence].shape[0] / ldf.shape[0]
-
-
-
+    number_above_confidence = ldf[ldf['ratio']>=percentage_confidence].shape[0]
+    total_number = ldf.shape[0]
+    percent_similar = 100*number_above_confidence/total_number
+    print("HERE ARE MY FINDINGS:\n--------------------")
+    time.sleep(2)
+    print(f'{number_above_confidence} out of {total_number} tweets are at or above {percentage_confidence}% similarity.')
+    print(f'Hence, @{username} is {percent_similar:.0f}% similar to @therealdonaldtrump!\nIn my expert opinion...')
+    time.sleep(2)
+    if percent_similar>50:
+        print("This person is Donald Trump! Confront them with the truth!")
+    elif percent_similar>20:
+        print("This person may be Donald Trump! Keep an eye on them :|")
+    else:
+        print("This person is probably not the Donald... for now...")
+    return None
 
 api = authentication(consumer_key, consumer_secret, callback_uri)
 username = input("Enter the username you suspect of being Donald Trump (public profiles only): @")
 user_corpus = user_data_parsing(api, username, max_tweets)
 trump_corpus = trump_data_parsing(trump_csv)
 similarity_scores = data_comparison(user_corpus, trump_corpus)
-verdict = user_similarity_verdict(similarity_scores, percentage_confidence=50)
-print(verdict)
+user_similarity_verdict(username, similarity_scores, percentage_confidence=70)
 
-
-# trump_fulltweetdata = pd.read_csv("mid20to2021_trump_tweets.csv")
-# # print(trump_fulltweetdata['text'][2])
-
-# trump_corpus = trump_fulltweetdata['text'].str.split("https", expand=True)
-# trump_corpus = trump_corpus[0]
-# # print(trump_corpus[0])
-
-# for val in trump_corpus:
-#     print(val)
-# print(type(trump_corpus[0]))
-# try:
-#     trump_corpus.dropna(inplace=True)
-# except:
-#     pass
-
-# l = []
-# for i in trump_corpus:
-#     for j in user_corpus:
-#         l.append(
-#             {
-#                 'ratio': fuzz.WRatio(i, j),
-#                 'TrumpText': i,
-#                 'UserText': j
-#             }
-#         )
-
-# percentconf = 90
-# ldf = pd.DataFrame(l)
-# ldf_sorted = ldf.sort_values(by="ratio", ascending=False)
-# print(ldf_sorted.head())
-# print(ldf_sorted[ldf_sorted['ratio']>=percentconf].shape[0], "out of", ldf_sorted.shape[0], f"at or above {percentconf}% similar")
-
-
-print(f"\nIt took {time.time()-start:.0f} seconds")
+print(f"\nThis operation took {time.time()-start:.0f} seconds")
